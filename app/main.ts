4ea24c55a14d847d86edcb16526df72372938776
Grégory Alvim <gregori.jagua@gmail.com>
2024-06-24 20:47:23 -0300

    pass 2st stage

--- app/main.ts
+++ app/main.ts
@@ -2,8 +2,13 @@ import * as net from "net";
 
 const server = net.createServer((socket) => {
   socket.on("close", () => {
+    socket.write(Buffer.from('HTTP/1.1 200 OK\r\n\r\n'))
     socket.end();
   });
 });
 
-server.listen(4221, "localhost");
+console.log("Logs from your program will appear here!");
+
+server.listen(4221, "localhost", () => {
+  console.log("Server is running on port 4221");
+});
