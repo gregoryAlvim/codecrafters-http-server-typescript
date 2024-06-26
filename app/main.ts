bc336fb975938abc7b6cd1edebd561bd7009ede4
Grégory Alvim <gregori.jagua@gmail.com>
2024-06-26 19:21:38 -0300

    test

--- app/main.ts
+++ app/main.ts
@@ -1,30 +1,35 @@
-import { request } from "http";
 import * as net from "net";
 
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
       const [rest, body] = data.toString().split('\r\n\r\n')
       const [status, ...headers] = rest.split("\r\n");
-      const [method, path, httpVersion] = status.split(" ");
 
-      if (path === "/") {
-        const response = "HTTP/1.1 200 OK\r\n\r\n"
+      const [method, rootPath, httpVersion] = status.split(" ");
+      const path = rootPath.split('/')[1]
+
+      const sendResponse = (response: string) => {
         socket.write(response)
-      }
-      else if (path.includes("echo/")) {
-        const query = path.split("/echo/")[1]
-        const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${query.length}\r\n\r\n${query}`
-        socket.write(response)
-      } else if (path === "/user-agent") {
-        const [_, userAgent] = headers[1].split(" ");
-        const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
-        socket.write(response)
-      } else {
-        const response = `HTTP/1.1 404 Not Found\r\n\r\n`
-        socket.write(response)
+        socket.end()
       }
 
-      socket.end()
+      switch (path) {
+        case "":
+          sendResponse("HTTP/1.1 200 OK\r\n\r\n")
+          break;
+        case "echo":
+          const stringParam = path.split("/echo/")[1]
+          sendResponse(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${stringParam.length}\r\n\r\n${stringParam}`)
+          break;
+        case "user-agent":
+          const [_, userAgent] = headers[1].split(" ");
+          sendResponse(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
+          break;
+      
+        default:
+          sendResponse("HTTP/1.1 404 Not Found\r\n\r\n")
+          break;
+      }
     })
 });
 
