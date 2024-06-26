d29c58ad8b4c82b9d784f3719575a1ad3f0e95d8
Grégory Alvim <gregori.jagua@gmail.com>
2024-06-25 21:21:43 -0300

    pass 4st stage

--- app/main.ts
+++ app/main.ts
@@ -9,7 +9,7 @@ const server = net.createServer((socket) => {
     socket.on('data', (data) => {
       const request = data.toString().split(' ')
       const path = request[1]
-      const stringParam = path.split("/")[1]
+      const stringParam = path.split("/")
       console.log(path, " ", stringParam)
       const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${stringParam.length}\r\n\r\n${stringParam}`
 
